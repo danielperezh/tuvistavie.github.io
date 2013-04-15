d5cd191d886e08a368020f3dbd3f03bc0ff80647
tuvistavie <tuvistavie@gmail.com>
2013-04-16 01:46:48 +0900

    Dynamically load sidebar articles.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,8 +1,13 @@
 class ApplicationController < ActionController::Base
     protect_from_forgery
     before_filter :set_fallbacks
+    before_filter :load_recent_posts
 
     def set_fallbacks
         Globalize.fallbacks = {:en => [:en, :ja], :ja => [:ja, :en] }
+    end
+
+    def load_recent_posts
+        @recent_posts = Article.limit(4).order("created_at ASC")
     end
 end
