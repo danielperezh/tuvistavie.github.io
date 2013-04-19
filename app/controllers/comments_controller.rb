9aee33feb7fa173b6169e2d6a66c8e783158e738
tuvistavie <tuvistavie@gmail.com>
2013-04-20 01:57:47 +0900

    Fix code for ruby 1.8 compatibility.

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -49,7 +49,7 @@ class CommentsController < ApplicationController
         format.html { redirect_to @comment, :notice => 'Comment was successfully created.' }
         format.json { render :json => @comment, :status => :created, :location => @comment }
       else
-        format.html { render action: "new" }
+        format.html { render :action => "new" }
         format.json { render :json => @comment.errors, :status => :unprocessable_entity }
       end
     end
@@ -65,7 +65,7 @@ class CommentsController < ApplicationController
         format.html { redirect_to @comment, :notice => 'Comment was successfully updated.' }
         format.json { head :no_content }
       else
-        format.html { render action: "edit" }
+        format.html { render :action => "edit" }
         format.json { render :json => @comment.errors, :status => :unprocessable_entity }
       end
     end
