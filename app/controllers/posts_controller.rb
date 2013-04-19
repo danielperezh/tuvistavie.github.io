590491888bcba7721ee766b6735f6bc0aa914a9e
tuvistavie <tuvistavie@gmail.com>
2013-04-20 01:54:52 +0900

    Fix code for ruby 1.8 compatibility.

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -65,11 +65,11 @@ class PostsController < ApplicationController
 
     respond_to do |format|
       if @post.save
-        format.html { redirect_to @post, notice: 'Post was successfully created.' }
-        format.json { render :json => @post, status: :created, location: @post }
+        format.html { redirect_to @post, :notice => 'Post was successfully created.' }
+        format.json { render :json => @post, :status => :created, location: @post }
       else
         format.html { render action: "new" }
-        format.json { render :json => @post.errors, status: :unprocessable_entity }
+        format.json { render :json => @post.errors, :status => :unprocessable_entity }
       end
     end
   end
@@ -81,11 +81,11 @@ class PostsController < ApplicationController
 
     respond_to do |format|
       if @post.update_attributes(params[:post])
-        format.html { redirect_to @post, notice: 'Post was successfully updated.' }
+        format.html { redirect_to @post, :notice => 'Post was successfully updated.' }
         format.json { head :no_content }
       else
         format.html { render action: "edit" }
-        format.json { render :json => @post.errors, status: :unprocessable_entity }
+        format.json { render :json => @post.errors, :status => :unprocessable_entity }
       end
     end
   end
