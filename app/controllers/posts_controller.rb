434dbe0f239e07432cd1fb68f478c0ce7a156dad
tuvistavie <tuvistavie@gmail.com>
2013-04-21 15:16:02 +0900

    Remove JSON support from Post controllers.

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -2,7 +2,6 @@ class PostsController < ApplicationController
   before_filter :authenticate_admin!, :except => [:index, :show]
 
   # GET /posts
-  # GET /posts.json
   def index
     if params[:tag].nil?
       posts = Post.with_translations(I18n.locale)
@@ -10,39 +9,21 @@ class PostsController < ApplicationController
       posts = Post.find_by_tag(params[:tag])
     end
 
-    @content_limit = Settings.posts['content_limit_' + I18n.locale.to_s]
+    @content_limit = Settings.posts["content_limit_#{I18n.locale.to_s}"]
 
     page = params[:page].nil? ? 1 : params[:page]
     @posts = posts.paginate(:page => page).order('posts.created_at DESC')
-
-    respond_to do |format|
-      format.html # index.html.erb
-      format.json { render :json => @posts }
-    end
   end
 
   # GET /posts/1
-  # GET /posts/1.json
   def show
     @post = Post.find(params[:id])
-
     @using_fallback = !@post.translated_locales.include?(I18n.locale)
-
-    respond_to do |format|
-      format.html # show.html.erb
-      format.json { render :json => @post }
-    end
   end
 
   # GET /posts/new
-  # GET /posts/new.json
   def new
     @post = Post.new
-
-    respond_to do |format|
-      format.html # new.html.erb
-      format.json { render :json => @post }
-    end
   end
 
   # GET /posts/1/edit
@@ -54,40 +35,30 @@ class PostsController < ApplicationController
   end
 
   # POST /posts
-  # POST /posts.json
   def create
     params[:post][:friendly_id] = params[:post][:title] if I18n.locale == :en
     tags_hash = params[:post].delete(:tags_attributes)
     @post = Post.new(params[:post])
     manage_tags(@post, tags_hash)
 
-    respond_to do |format|
-      if @post.save
-        format.html { redirect_to @post, :notice => 'Post was successfully created.' }
-        format.json { render :json => @post, :status => :created, :location => @post }
-      else
-        format.html { render :action => "new" }
-        format.json { render :json => @post.errors, :status => :unprocessable_entity }
-      end
+    if @post.save
+      redirect_to @post, :notice => 'Post was successfully created.'
+    else
+      render :action => "new"
     end
   end
 
   # PUT /posts/1
-  # PUT /posts/1.json
   def update
     params[:post][:friendly_id] = params[:post][:title] if I18n.locale == :en
     tags_hash = params[:post].delete(:tags_attributes)
     @post = Post.find(params[:id])
     manage_tags(@post, tags_hash)
 
-    respond_to do |format|
-      if @post.update_attributes(params[:post])
-        format.html { redirect_to @post, :notice => 'Post was successfully updated.' }
-        format.json { head :no_content }
-      else
-        format.html { render :action => "edit" }
-        format.json { render :json => @post.errors, :status => :unprocessable_entity }
-      end
+    if @post.update_attributes(params[:post])
+      redirect_to @post, :notice => 'Post was successfully updated.'
+    else
+      render :action => "edit"
     end
   end
 
