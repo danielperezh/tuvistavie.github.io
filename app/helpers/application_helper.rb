8b03bd2e363aa6f30b73857d4b57998463647408
tuvistavie <tuvistavie@gmail.com>
2013-04-21 13:56:57 +0900

    Remove nil safe cloudinary helper.

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -4,9 +4,4 @@ module ApplicationHelper
         url_info['locale'] = locale
         url_info
     end
-
-    def safe_cl_image_tag(public_id, **params)
-        public_id = '' if public_id.nil?
-        cl_image_tag(public_id, **params)
-    end
 end
