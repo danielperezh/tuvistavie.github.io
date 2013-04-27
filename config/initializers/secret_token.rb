eb34f694ec6cc6d3302cef2508c9d2954a73b639
tuvistavie <tuvistavie@gmail.com>
2013-04-27 20:50:41 +0900

    Remove secret token file.

--- config/initializers/secret_token.rb
+++
@@ -1,7 +0,0 @@
-# Be sure to restart your server when you modify this file.
-
-# Your secret key for verifying the integrity of signed cookies.
-# If you change this key, all old signed cookies will become invalid!
-# Make sure the secret is at least 30 characters and all random,
-# no regular words or you'll be exposed to dictionary attacks.
-Blog::Application.config.secret_token = 'a4538b146b66b329dfb0a454661419f8eaa58db084d2b5f4a2b4246b9f5a62f1d9942ca0e096b8f567c43167efc38a8f7940f90655bc2814a6c5ccd19fd74a60'
