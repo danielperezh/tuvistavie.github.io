b0078fb4ff906b2f5fa8930f2cc17fc237367cf9
tuvistavie <tuvistavie@gmail.com>
2013-05-13 12:14:00 +0900

    Remove useless output.

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -5,7 +5,6 @@ class CommentsController < ApplicationController
   def index
     post = Post.find(params[:post_id])
     @comments = post.comments
-    puts YAML::dump(@comments)
     render :json => @comments
   end
 
